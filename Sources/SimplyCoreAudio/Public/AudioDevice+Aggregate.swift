2d546decc3ba15161c1a3359c3ec70d231f3f78c
ichizok <gclient.gaap@gmail.com>
2023-03-10 15:22:12 +0900

    WIP Add AudioDevice.allSubDevices
    Remove setter of ownedAggregateDevices

--- Sources/SimplyCoreAudio/Public/AudioDevice+Aggregate.swift
+++ Sources/SimplyCoreAudio/Public/AudioDevice+Aggregate.swift
@@ -20,22 +20,8 @@ public extension AudioDevice {
     ///
     /// - Returns: An array of `AudioDevice` objects.
     var ownedAggregateDevices: [AudioDevice]? {
-        get {
-            guard let ownedObjectIDs = ownedObjectIDs else { return nil }
-            return ownedObjectIDs.compactMap { AudioDevice.lookup(by: $0) }
-        }
-
-        set {
-            guard isAggregateDevice else { return }
-
-            guard var address = validAddress(selector: kAudioAggregateDevicePropertyFullSubDeviceList) else { return }
-            let newValue = (newValue ?? []).compactMap { $0.uid }.unique()
-
-            let size = UInt32(MemoryLayout<CFArray>.size)
-            var value = newValue as CFArray
-
-            let _ = AudioObjectSetPropertyData(objectID, &address, UInt32(0), nil, size, &value)
-        }
+        guard let ownedObjectIDs = ownedObjectIDs else { return nil }
+        return ownedObjectIDs.compactMap { AudioDevice.lookup(by: $0) }
     }
 
     /// All the subdevices of this aggregate device that support input
@@ -58,6 +44,57 @@ public extension AudioDevice {
         }
     }
 
+    /// All the subdevices of this aggregate device
+    ///
+    /// - Returns: An array of `AudioDevice` objects.
+    var allSubDevices: [AudioDevice]? {
+        get {
+            guard isAggregateDevice else { return nil }
+
+            guard var address = validAddress(selector: kAudioAggregateDevicePropertyFullSubDeviceList) else { return nil }
+
+            var size = UInt32(MemoryLayout<CFArray>.size)
+            var value: CFArray? = nil
+
+            let status = AudioObjectGetPropertyData(objectID, &address, UInt32(0), nil, &size, &value)
+            guard noErr == status, let value = value else { return nil }
+
+            return ((value as NSArray) as! [String]).compactMap { AudioDevice.lookup(by: $0) }
+        }
+
+        set {
+            guard isAggregateDevice else { return }
+
+            guard var address = validAddress(selector: kAudioAggregateDevicePropertyFullSubDeviceList) else { return }
+            let newValue = (newValue ?? []).compactMap { $0.uid }.unique()
+
+            let size = UInt32(MemoryLayout<CFArray>.size)
+            var value = newValue as CFArray
+
+            let _ = AudioObjectSetPropertyData(objectID, &address, UInt32(0), nil, size, &value)
+        }
+    }
+
+    /// All the subdevices of this aggregate device that support input
+    ///
+    /// - Returns: An array of `AudioDevice` objects.
+    var allInputSubDevices: [AudioDevice]? {
+        allSubDevices?.filter {
+            guard let channels = $0.layoutChannels(scope: .input) else { return false }
+            return channels > 0
+        }
+    }
+
+    /// All the subdevices of this aggregate device that support output
+    ///
+    /// - Returns: An array of `AudioDevice` objects.
+    var allOutputSubDevices: [AudioDevice]? {
+        allSubDevices?.filter {
+            guard let channels = $0.layoutChannels(scope: .output) else { return false }
+            return channels > 0
+        }
+    }
+
     /// - Returns: *(optional)* A `String` with the audio device `UID`.
     var mainSubDevice: AudioDevice? {
         get {
@@ -71,8 +108,11 @@ public extension AudioDevice {
 
             guard let address = validAddress(selector: kAudioAggregateDevicePropertyMainSubDevice) else { return }
             guard let uid = newValue?.uid else { return }
-            if !ownedAggregateDevices!.contains(where: { $0.uid == uid }) {
-                ownedAggregateDevices! += [newValue!]
+
+            if allSubDevices == nil {
+                allSubDevices = [newValue!]
+            } else {
+                allSubDevices!.insert(newValue!, at: 0)
             }
             let _ = setProperty(address: address, value: uid)
         }
@@ -96,8 +136,11 @@ public extension AudioDevice {
 
             guard let address = validAddress(selector: kAudioAggregateDevicePropertyClockDevice) else { return }
             guard let uid = newValue?.uid else { return }
-            if !ownedAggregateDevices!.contains(where: { $0.uid == uid }) {
-                ownedAggregateDevices! += [newValue!]
+
+            if allSubDevices == nil {
+                allSubDevices = [newValue!]
+            } else {
+                allSubDevices!.insert(newValue!, at: 0)
             }
             let _ = setProperty(address: address, value: uid)
         }
